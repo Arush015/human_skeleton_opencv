25bfe011f5e77107b5388c8814de9a1351e7eb07
Arush015 <57981342+Arush015@users.noreply.github.com>
2020-08-11 17:57:44 +0530

    Add files via upload

---
+++ main_image.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+protoFile = "pose/coco/pose_deploy_linevec.prototxt"
+weightsFile = "pose/coco/pose_iter_440000.caffemodel"
+nPoints = 18
+POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
+
+print("[INFO] Process starts")
+frame = cv2.imread("single.jpeg")
+frameWidth = frame.shape[1]
+frameHeight = frame.shape[0]
+threshold = 0.1
+
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (368, 368), (0, 0, 0), swapRB=False, crop=False)
+net.setInput(inpBlob)
+output = net.forward()
+
+H = output.shape[2]
+W = output.shape[3]
+
+# Empty list to store the detected keypoints
+points = []
+
+for i in range(nPoints):
+    # confidence map of corresponding body's part.
+    probMap = output[0, i, :, :]
+
+    # Find global maxima of the probMap.
+    minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+    
+    # Scale the point to fit on the original image
+    x = (frameWidth * point[0]) / W
+    y = (frameHeight * point[1]) / H
+
+    if prob > threshold : 
+        # Add the point to the list if the probability is greater than the threshold
+        points.append((int(x), int(y)))
+    else :
+        points.append(None)
+
+# Draw Skeleton
+for pair in POSE_PAIRS:
+    partA = pair[0]
+    partB = pair[1]
+
+    if points[partA] and points[partB]:
+        cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
+        cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+
+cv2.imshow('Output', frame)
+cv2.imwrite('Output.jpg', frame)
+print("[INFO] Process completed...")  
+print("[INFO] Close window to exit")
+cv2.waitKey(0)
+
---
+++ main_video.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+
+protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
+nPoints = 15
+POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
+
+threshold = 0.1
+
+print("[INFO] Process starts")
+cap = cv2.VideoCapture('sample.mp4')
+hasFrame, frame = cap.read()
+vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
+print("[INFO] Video is being processed...")
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+while cv2.waitKey(1) < 0:
+    hasFrame, frame = cap.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
+
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (368, 368),(0, 0, 0), swapRB=False, crop=False)
+    net.setInput(inpBlob)
+    output = net.forward()
+
+    H = output.shape[2]
+    W = output.shape[3]
+    # Empty list to store the detected keypoints
+    points = []
+
+    for i in range(nPoints):
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
+
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        
+        # Scale the point to fit on the original image
+        x = (frameWidth * point[0]) / W
+        y = (frameHeight * point[1]) / H
+
+        if prob > threshold : 
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else :
+            points.append(None)
+
+    # Draw Skeleton
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+
+        if points[partA] and points[partB]:
+            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
+            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+
+    cv2.putText(frame, "OpenCV", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    cv2.imshow('Output', frame)
+    vid_writer.write(frame)
+    
+print("[INFO] Process completed...")    
+vid_writer.release()
